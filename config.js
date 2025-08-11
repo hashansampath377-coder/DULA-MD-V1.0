c5cd582c54b2a708bc7a7e04fd5c49205fe72cd5
PODDAe <camalkaakash2@gmail.com>
2025-08-11 09:18:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://ibb.co/VYTLfPSv",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/PODDAe/DULA-MD-V1.0/blob/main/IMAGES/DULA-MD-LOGO.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DULA-MD Is Alive Now😍*",
 BOT_OWNER: '94752978237',  // Replace with the owner's phone number
 
