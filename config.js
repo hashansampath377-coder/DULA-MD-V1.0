ab0f311cd4918c09b00ca47907029d3352d21d7f
hashansampath377-coder <hashansampath377@gmail.com>
2025-08-11 13:49:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "YGtHlATT#CwZkTOsXAaPyBLrkBW7VTgSUJhJ7RSnPktev-H31cKg",
+SESSION_ID: process.env.SESSION_ID || "oO8D3DZD#lWaSYc_KpoJbWNCG8An7EKgWqy3prDwTxsACVIBBKmg",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/PODDAe/DULA-MD-V1.0/blob/main/IMAGES/DULA-MD-LOGO.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DULA-MD Is Alive Now😍*",
-BOT_OWNER: '94752978237',  // Replace with the owner's phone number
+BOT_OWNER: '94782543893',  // Replace with the owner's phone number
 
 
 
